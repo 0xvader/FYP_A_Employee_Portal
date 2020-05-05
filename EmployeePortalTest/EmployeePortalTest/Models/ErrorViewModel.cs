5acf6cae54eda6b638325ce8dbcf161fa39a211b
0xvader <alexhaide55@hotmail.com>
2020-05-05 22:37:19 +0800

    EmployeeLoginTest

---
+++ EmployeePortalTest/EmployeePortalTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeePortalTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
