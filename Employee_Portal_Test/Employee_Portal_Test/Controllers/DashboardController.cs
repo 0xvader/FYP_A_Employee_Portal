3028192b418a1dd2b62656b2b02f810d1cb2b36f
0xvader <alexhaider55@hotmail.com>
2020-06-23 08:19:35 +0800

    Fix controller

---
+++ Employee_Portal_Test/Employee_Portal_Test/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_Portal_Test.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Dashboard1()
+        {
+            return View();
+        }
+        public IActionResult Dashboard2()
+        {
+            return View();
+        }
+        public IActionResult Dashboard3()
+        {
+            return View();
+        }
+        public IActionResult View1()
+        {
+            return View();
+        }
+    }
+}
