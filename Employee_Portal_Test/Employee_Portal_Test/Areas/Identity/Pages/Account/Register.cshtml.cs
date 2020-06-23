619ba7a4650f89f7975e0d9172f5bd616caa7792
Munyoomin <joshuamun31@gmail.com>
2020-06-23 13:49:01 +0800

    Code for Authorization

--- Employee_Portal_Test/Employee_Portal_Test/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Employee_Portal_Test/Employee_Portal_Test/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -24,17 +24,19 @@ namespace Employee_Portal_Test.Areas.Identity.Pages.Account
         private readonly UserManager<Employee_Portal_TestUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
-
+        private readonly RoleManager<IdentityRole> _roleManager;
         public RegisterModel(
             UserManager<Employee_Portal_TestUser> userManager,
             SignInManager<Employee_Portal_TestUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _roleManager = roleManager;
         }
 
         [BindProperty]
@@ -71,18 +73,21 @@ namespace Employee_Portal_Test.Areas.Identity.Pages.Account
             [Display(Name = "Confirm Password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            public string Name { get; set; }
         }
 
-        public async Task OnGetAsync(string returnUrl = null)
+        public void OnGet(string returnUrl = null)
         {
+            ViewData["roles"] = _roleManager.Roles.ToList();
             ReturnUrl = returnUrl;
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl = returnUrl ?? Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            var role = _roleManager.FindByEmpnoAsync(Input.Name).Result;
             if (ModelState.IsValid)
             {
                 var user = new Employee_Portal_TestUser { UserName = Input.Email, Email = Input.Email, Name = Input.Name, Empno = Input.Empno  };
@@ -90,27 +95,30 @@ namespace Employee_Portal_Test.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    await _userManager.AddToRoleAsync(user, role.Name);
 
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code },
-                        protocol: Request.Scheme);
+                    //    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    //    var callbackUrl = Url.Page(
+                    //        "/Account/ConfirmEmail",
+                    //        pageHandler: null,
+                    //        values: new { area = "Identity", userId = user.Id, code = code },
+                    //        protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    //    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+                    //    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    //    {
+                    //        return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
+                    //    }
+                    //    else
+                    //    {
+                    //        await _signInManager.SignInAsync(user, isPersistent: false);
+                    //        return LocalRedirect(returnUrl);
+                    //    }
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
                 }
                 foreach (var error in result.Errors)
                 {
--- Employee_Portal_Test/Employee_Portal_Test/Startup.cs
+++ Employee_Portal_Test/Employee_Portal_Test/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +24,8 @@ namespace Employee_Portal_Test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddRoles<IdentityRole>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
