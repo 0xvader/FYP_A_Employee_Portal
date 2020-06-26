32508ec3e677a36b8ea5c6e80827303d79f5593e
Munyoomin <joshuamun31@gmail.com>
2020-06-27 04:44:26 +0800

    Forgot password and email confirmation working

---
+++ Employee_Portal_Test(moon)/Employee_Portal_Test/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Employee_Portal_Test.Areas.Identity.Data;
+using Employee_Portal_Test.Data;
+using Employee_Portal_Test.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+
+[assembly: HostingStartup(typeof(Employee_Portal_Test.Areas.Identity.IdentityHostingStartup))]
+namespace Employee_Portal_Test.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup 
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Employee_Portal_TestDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Employee_Portal_TestDbContextConnection")));
+                 
+                services.AddDefaultIdentity<Employee_Portal_TestUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = true;//true
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<Employee_Portal_TestDbContext>();
+
+            });
+        }
+    }
+}
