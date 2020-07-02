b5c3a430dcfcbccb88fa771fb049515b7606f1cf
0xvader <alexhaider55@hotmail.com>
2020-07-02 20:25:00 +0800

    Branched

---
+++ Employee_Portal_Test Alex/Employee_Portal_Test/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Employee_Portal_Test.Areas.Identity.Data;
+using Employee_Portal_Test.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Employee_Portal_Test.Areas.Identity.IdentityHostingStartup))]
+namespace Employee_Portal_Test.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Employee_Portal_TestDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Employee_Portal_TestDbContextConnection")));
+                 
+                services.AddDefaultIdentity<Employee_Portal_TestUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = true;//true
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<Employee_Portal_TestDbContext>();
+            });
+        }
+    }
+}
