628416f9f0875674e4302b29f63b2ca6c6b152fe
0xvader <48783579+0xvader@users.noreply.github.com>
2020-11-29 12:22:42 +0800

    Final Stable release

---
+++ FYP B/Alex/Employee_Portal_Test_241120_6pm/Employee_Portal_Test/Migrations/bcck_temp/20201125155027_changeDB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Employee_Portal_Test.Migrations.bcck_temp
+{
+    public partial class changeDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DocType",
+                table: "Doc",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DocType",
+                table: "Doc");
+        }
+    }
+}
---
+++ FYP B/Alex/Employee_Portal_Test_241120_6pm/Employee_Portal_Test/Models/doc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Portal_Test.Models
+{
+    public partial class doc
+    {
+        [Key]
+        public int docID { get; set; }
+
+        public string title { get; set; }
+        public string docpath { get; set; }
+
+        public string EMPNO { get; set; }
+        public string DocType { get; set; }
+    }
+}
